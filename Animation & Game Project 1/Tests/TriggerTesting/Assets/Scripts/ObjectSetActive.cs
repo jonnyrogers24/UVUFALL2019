df54c82438740c21213fca46edbc939f6c6c7b08
Unknown <jonnyrogersanimation@gmail.com>
2019-09-19 23:52:02 -0600

    trigger test

---
+++ Animation & Game Project 1/Tests/TriggerTesting/Assets/Scripts/ObjectSetActive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSetActive : MonoBehaviour
+{
+   
+
+  private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Triggered");
+        }
+    }
+}
