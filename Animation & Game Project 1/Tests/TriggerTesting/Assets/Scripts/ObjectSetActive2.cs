51bb5ebaa3534f05bb84931c0ffabc6231de6798
Unknown <jonnyrogersanimation@gmail.com>
2019-09-23 19:25:29 -0600

    Set Highlight Active After 2 Triggers Are Passed Through

--- Animation & Game Project 1/Tests/TriggerTesting/Assets/Scripts/ObjectSetActive2.cs
+++ Animation & Game Project 1/Tests/TriggerTesting/Assets/Scripts/ObjectSetActive2.cs
@@ -2,23 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectSetActive : MonoBehaviour
+public class ObjectSetActive2 : MonoBehaviour
 {
 
-    public GameObject trigger2; 
     public GameObject highlight;
-
-    private void Start()
-    {
-        trigger2.SetActive(false);
-        highlight.SetActive(false);
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Reverse_Gear"))
         {
-            trigger2.SetActive(true); 
+            highlight.SetActive(true); 
         }      
     }
 }
