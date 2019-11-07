958307687e9e4ae67d4aa6dfbdf26f2f88db875e
Jonathon Rogers <jonnyrogersanimation@gmail.com>
2019-11-07 13:09:12 -0700

    PyCharm Settings

--- Scripting for Animation/PycharmScripts/Scripts/test.py
+++ Scripting for Animation/PycharmScripts/Scripts/test.py
@@ -1,6 +1,34 @@
 import maya.cmds as cmds
 
-cmds.polysphere(axis=[1, 0, 0], radius=5, name='Ball', constructionHistory=True)
-cmds.polyCube(d=3, h=1,w=5, n='Box')
+sels = cmds.ls(sl=True)
+print sels[0]
 
-cmds.xform('Ball', ws=True, translation=[5, 8, 10])
+numInt = 3
+numFloat = 6.3
+
+print numInt
+print numFloat
+
+sels = "hello"
+print sels
+
+students = [['bill', 'bob', 'candace', 'rebecca'],
+            [100,98,67,37],
+            ['Clayton', 'Clayton', 'Clayton','Clayton']]
+print students[0][3], 'respects' , students[2][0]
+
+for i in range(len(students[0])):
+    print 'Name:', students[0][i]
+    print 'Score', students[1][i]
+    print 'Teacher', students[2][i]
+    print '-----------------------'
+print 'Complete'
+
+for name in students[0]:
+    print name, 'is great!'
+
+for i in range(len(students)):
+    print students[i]
+    print i
+
+print "done"
