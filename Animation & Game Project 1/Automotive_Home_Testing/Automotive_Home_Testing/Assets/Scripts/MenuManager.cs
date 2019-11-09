4813cd082aa867d5a8400cc6d6677b07982cb7ec
Jonny Rogers <jonnyrogersanimation@gmail.com>
2019-11-09 01:11:10 -0700

    main menu semi working(basics)

--- Animation & Game Project 1/Automotive_Home_Testing/Automotive_Home_Testing/Assets/Scripts/MenuManager.cs
+++ Animation & Game Project 1/Automotive_Home_Testing/Automotive_Home_Testing/Assets/Scripts/MenuManager.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    public GameObject mainMenu; 
-    public GameObject resetTimerButton
+    public GameObject mainMenu;
+
+    public GameObject resetTimerButton;
     // Start is called before the first frame update
     void Start()
     {
+        mainMenu.SetActive(true);
+        resetTimerButton.SetActive(false);
         
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public void Menu()
+    {
+        mainMenu.SetActive(false);
+        resetTimerButton.SetActive(true);
+    }
+    
+    public void SubMenu()
     {
         
     }
